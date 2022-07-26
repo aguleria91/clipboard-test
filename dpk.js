7a2dbe23763a9361bc86bcc3779d17d1592ce95e
Aditya Guleria <aditya@koresoft.in>
2022-07-26 12:59:26 +0530

    commit

--- dpk.js
+++ dpk.js
@@ -1,28 +1,42 @@
 const crypto = require("crypto");
 
+/**
+ * Function to return the candidate hash key based on the event.
+ * @param {*} event 
+ * @returns {string | number} candidate
+ */
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
+
   let candidate;
 
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
+      candidate = event.partitionKey; // covered
     } else {
       const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      candidate = crypto.createHash("sha3-512").update(data).digest("hex"); // covered
     }
   }
+
+  candidate = candidateHashKey(candidate);
+
+  return candidate;
+};
+
+function candidateHashKey(candidate) {
+  const TRIVIAL_PARTITION_KEY = "0";
+  const MAX_PARTITION_KEY_LENGTH = 256;
 
   if (candidate) {
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    candidate = TRIVIAL_PARTITION_KEY; // covered
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
   return candidate;
-};
+}
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,44 @@
 const { deterministicPartitionKey } = require("./dpk");
+const { candidateHashKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("Returns the candidate if event has partion key", () => {
+    const testEvent = {
+      name: "user1",
+      partitionKey: "check_this_key"
+    }
+    const trivialKey = deterministicPartitionKey(testEvent);
+    expect(trivialKey).toBe(testEvent.partitionKey);
+  });
+
+  it("Returns the candidate if event is without partion key", () => {
+    const testEvent = {
+      name: "user1"
+    }
+    const stringifiedEvent = JSON.stringify(testEvent);
+
+    const expectedTrivialKey = crypto.createHash("sha3-512").update(stringifiedEvent).digest("hex");
+    const trivialKey = deterministicPartitionKey(testEvent);
+    expect(trivialKey).toBe(expectedTrivialKey);
+  });
+
+
 });
+
+
+describe("candidateHashKey", () => {
+  it("Type check for the candidate ", () => {
+    const candidate = 12453
+    const stringifiedCandidate = JSON.stringify(candidate);
+
+    const candidateKey = candidateHashKey(candidate);
+    console.log("candidateKey-", candidateKey)
+    expect(candidateKey).toBe(stringifiedCandidate);
+  });
+})
